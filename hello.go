72706f9d4e07ebf51af9f3942c775be4583c951b
Your Name <ayeshazakria22@gmail.com>
2020-11-09 10:58:22 -0800

    new commits

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+  
+import "fmt"
+
+func main() {
+  fmt.Println("Hello everyone in the world")
+  fmt.Println("we are having our lectures on sundays")
+  fmt.Println("we are enjoying it")
+}
