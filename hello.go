a2be9671bdda3ed918f9fd096b8bef94f2e54f2d
Ayeshazakria <58141121+Ayeshazakria@users.noreply.github.com>
2020-11-10 05:18:31 -0800

    Delete hello.go

--- hello.go
+++
@@ -1,9 +0,0 @@
-package main
-  
-import "fmt"
-
-func main() {
-  fmt.Println("Hello everyone in the world")
-  fmt.Println("we are having our lectures on sundays")
-  fmt.Println("we are enjoying it")
-}
